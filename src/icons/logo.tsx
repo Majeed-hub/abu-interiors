ed690819e427f30fc38512665099fc2b6da700c8
Majeed-hub <majeedbkl65@gmail.com>
2024-11-27 21:33:33 +0530

    logo config

--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo: React.FC = () => {
   return (
     <div>
-      <img src="/abu-interiors.png" alt="Abu Interiors Logo" />
+      <img src="/abu-interiors.png" alt="Abu Interiors Logo" height="200px" width="200px" />
     </div>
   );
 };
