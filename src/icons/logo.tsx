1aff2282e4b730ab2f9361718fce59d1b8d1f62f
Majeed-hub <majeedbkl65@gmail.com>
2024-11-27 21:40:52 +0530

    changed logo

--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Logo: React.FC = () => {
   return (
-    <div>
-      <img src="/abu-interiors.png" alt="Abu Interiors Logo" height="200px" width="200px" />
+    <div className="flex items-center space-x-3">
+      <img
+        src="/abu-interiors.png"
+        alt="Abu Interiors Logo"
+        className="h-16 w-16 object-contain" // Tailwind classes for styling image
+      />
+      <h2 className="text-2xl font-norwester text-blue-600">Abu Interiors</h2> {/* Tailwind classes for text */}
     </div>
   );
 };
