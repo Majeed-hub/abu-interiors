c3bf419a596fb1614bb74f27ce9e079c6ee3fe35
Majeed-hub <majeedbkl65@gmail.com>
2024-11-27 22:11:03 +0530

    changes contact info

--- src/components/contact-box.tsx
+++ src/components/contact-box.tsx
@@ -7,15 +7,15 @@ const ContactInfo = () => {
     <div className="col-span-1 bg-primary-300 rounded-[30px] px-8 py-[76px] flex flex-col gap-[30px] w-full">
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Mail /> info@yourdomain.com
+        <Mail /> sales@abuinteriors.com
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Phone /> +1 (378) 400-1234
+        <Phone /> +91 7337830078
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Web /> www.yourdomain.com
+        <Web /> www.abuinteriors.com
       </h5>
     </div>
   );
