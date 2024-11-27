93c4f7c69a0068161cef4d5f66c791dc1832726c
Majeed-hub <majeedbkl65@gmail.com>
2024-11-27 20:57:50 +0530

    chnaged astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions"; // Netlify adapter
+import ViteSvgr from "vite-plugin-svgr"; // Vite SVG plugin
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,15 +11,7 @@ export default defineConfig({
   adapter: netlify(),
   vite: {
     plugins: [
-      {
-        name: "vite-plugin-svgr",
-        transform: (code, id) => {
-          if (id.endsWith(".svg")) {
-            return require("@svgr/webpack").loader(id);
-          }
-          return code;
-        },
-      },
+      ViteSvgr(), // Add the Vite SVG plugin
     ],
   },
 });
