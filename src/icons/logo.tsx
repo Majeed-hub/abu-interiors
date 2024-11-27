24f005d727880e894e22e54ab27460e7e80f32ec
Majeed-hub <majeedbkl65@gmail.com>
2024-11-27 23:22:30 +0530

    added norwester

--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -6,9 +6,9 @@ const Logo: React.FC = () => {
       <img
         src="/abu-interiors.png"
         alt="Abu Interiors Logo"
-        className="h-16 w-16 object-contain" // Tailwind classes for styling image
+        className="h-16 w-16 object-contain" // Image styling
       />
-      <h2 className="text-2xl text-[#332F59] bold font-norwester">Abu Interiors</h2> {/* Tailwind classes for text */}
+      <h2 className="text-2xl text-[#332F59] font-bold font-norwester">Abu Interiors</h2> {/* Text styling */}
     </div>
   );
 };
